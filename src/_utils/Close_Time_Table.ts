4d0cd5fe5fd8dea2b6ee5417d9d0e0529d518ace
Muhammed21 <mcavus048@gmail.com>
2025-03-12 20:12:32 +0100

    :hammer: Deploiement Vercel avec la connexion ainsi que l'API fetch review prete

--- src/_utils/Close_Time_Table.ts
+++ src/_utils/Close_Time_Table.ts
@@ -1,5 +1,8 @@
+// Jour fermé à adapter si besoin
 export const timeTable = ["mercredi"];
 
+// Horaire d'ouverture à adapter si besoin
 export const openHour = 11;
 
+// Horaire de fermeture à adapter si besoin
 export const closeHour = 24;
