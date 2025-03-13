97255dac85d20c284a8760da51a939b18cb80fdb
Muhammed21 <mcavus048@gmail.com>
2025-03-13 23:09:43 +0100

    :white_check_mark: Correction des bugs (next.config.ts)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*", // Allow images from all domains
-      },
-    ],
-  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
