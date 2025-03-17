d6a524b8597be38b70cd95c9476a0b559e5c9cd7
RomainLENTZ <romain.lentz74@gmail.com>
2025-03-17 13:19:56 +0100

    Favicon Ajouter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr" className="scroll-smooth">
-      <Head />
+      <Head>
+        <title>Alibaba Kebab - Annecy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
